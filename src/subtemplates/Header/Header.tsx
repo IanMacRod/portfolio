62ea6a387c74578bf0a058f20dfe95c52e80d8c8
IanMacRod <ianrodrigues.arc@gmail.com>
2023-06-17 19:49:48 -0300

    subtemplate started

---
+++ src/subtemplates/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Header = () => {
+    return(
+        <header>
+            <div className="logo">Logotipo</div>
+            <div className="menu"></div>
+        </header>
+    )
+}
+
+export default Header
